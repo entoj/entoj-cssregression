a63f80b21eda1d33a8735f744a5711095b01c964
Christian Auth <c.auth@syzygy.de>
2018-01-26 23:27:53 +0100

    Adds tasks

---
+++ source/task/CompareTask.js
@@ -0,0 +1,197 @@
+'use strict';
+
+/**
+ * Requirements
+ * @ignore
+ */
+const EntitiesTask = require('entoj-system').task.EntitiesTask;
+const GlobalRepository = require('entoj-system').model.GlobalRepository;
+const CliLogger = require('entoj-system').cli.CliLogger;
+const ErrorHandler = require('entoj-system').error.ErrorHandler;
+const VinylFile = require('vinyl');
+const streamBuffers = require('stream-buffers');
+const co = require('co');
+const fs = require('co-fs-extra');
+const PNG = require('pngjs').PNG;
+const pixelmatch = require('pixelmatch');
+
+
+/**
+ * @memberOf task
+ */
+class CompareTask extends EntitiesTask
+{
+    /**
+     * @inheritDoc
+     */
+    static get injections()
+    {
+        return { 'parameters': [CliLogger, GlobalRepository] };
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    static get className()
+    {
+        return 'task/CompareTask';
+    }
+
+
+    /**
+     * @protected
+     * @returns {Promise<PNG>}
+     */
+    readPng(filename)
+    {
+        const promise = new Promise((resolve) =>
+        {
+            const result = new PNG();
+            const data = fs.readFileSync(filename);
+            result.parse(data, () => resolve(result));
+        });
+        return promise;
+    }
+
+
+    /**
+     * @protected
+     * @returns {Promise<Buffer>}
+     */
+    writePng(filename, png)
+    {
+        const promise = new Promise((resolve) =>
+        {
+            const bufferStream = new streamBuffers.WritableStreamBuffer();
+            bufferStream.on('finish', function()
+            {
+                const file = new VinylFile(
+                    {
+                        path: filename,
+                        contents: bufferStream.getContents()
+                    });
+                resolve(file);
+            });
+            png.pack()
+                .pipe(bufferStream);
+        });
+        return promise;
+    }
+
+
+    /**
+     * @protected
+     * @returns {Promise<Array>}
+     */
+    prepareParameters(buildConfiguration, parameters)
+    {
+        const parent = super.prepareParameters(buildConfiguration, parameters);
+        const scope = this;
+        const promise = co(function*()
+        {
+            const params = yield parent;
+            params.query = params.query || '*';
+            params.threshold = params.threshold || 1;
+            return params;
+        }).catch(ErrorHandler.handler(scope));
+        return promise;
+    }
+
+
+    /**
+     * @returns {Promise<Array<VinylFile>>}
+     */
+    processEntity(entity, buildConfiguration, parameters)
+    {
+        /* istanbul ignore next */
+        if (!entity)
+        {
+            this.logger.warn(this.className + '::processEntity - No entity given');
+            return Promise.resolve(false);
+        }
+
+        const scope = this;
+        const promise = co(function *()
+        {
+            // Prepare
+            const result = [];
+            const params = yield scope.prepareParameters(buildConfiguration, parameters);
+
+            // Get suite
+            const testSuite = entity.testSuites.findBy(
+                {
+                    name: 'cssregression',
+                    site: entity.id.site
+                });
+
+            // Compare screenshots
+            if (testSuite)
+            {
+                testSuite.isValid = true;
+                testSuite.ok = 0;
+                testSuite.failed = 0;
+                for (const testCase of testSuite.tests)
+                {
+                    const referenceExists = yield fs.exists(testCase.referenceImagePath);
+                    const testExists = yield fs.exists(testCase.testImagePath);
+                    if (!referenceExists || !testExists)
+                    {
+                        testCase.isValid = false;
+                        testSuite.failed++;
+                        testSuite.isValid = false;
+                    }
+                    else
+                    {
+                        const referenceImage = yield scope.readPng(testCase.referenceImagePath);
+                        const testImage = yield scope.readPng(testCase.testImagePath);
+                        if (referenceImage.width == 0 ||
+                            referenceImage.height == 0 ||
+                            referenceImage.width != testImage.width ||
+                            referenceImage.height != testImage.height)
+                        {
+                            testCase.isValid = false;
+                            testSuite.failed++;
+                            testSuite.isValid = false;
+                        }
+                        else
+                        {
+                            const differenceImage = new PNG(
+                                {
+                                    width: referenceImage.width,
+                                    height: referenceImage.height
+                                });
+                            const result = pixelmatch(referenceImage.data, testImage.data, differenceImage.data,
+                                referenceImage.width, referenceImage.height, { threshold: 0.1 });
+                            const differenceFile = yield scope.writePng(testCase.differenceImagePath, differenceImage);
+                            if (differenceFile)
+                            {
+                                result.push(differenceFile);
+                            }
+                            if (result > params.threshold)
+                            {
+                                testCase.isValid = false;
+                                testSuite.failed++;
+                                testSuite.isValid = false;
+                            }
+                            else
+                            {
+                                testCase.isValid = true;
+                                testSuite.ok++;
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }).catch(ErrorHandler.handler(scope));
+        return promise;
+    }
+}
+
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports.CompareTask = CompareTask;
---
+++ source/task/ScreenshotTask.js
@@ -0,0 +1,199 @@
+'use strict';
+
+/**
+ * Requirements
+ * @ignore
+ */
+const Screenshot = require('../utils/Screenshot.js').Screenshot;
+const EntitiesTask = require('entoj-system').task.EntitiesTask;
+const GlobalRepository = require('entoj-system').model.GlobalRepository;
+const CliLogger = require('entoj-system').cli.CliLogger;
+const ErrorHandler = require('entoj-system').error.ErrorHandler;
+const VinylFile = require('vinyl');
+const co = require('co');
+const fs = require('fs');
+
+
+/**
+ * @memberOf task
+ */
+class ScreenshotTask extends EntitiesTask
+{
+    /**
+     * @param {cli.CliLogger} cliLogger
+     * @param {model.GlobalRepository} globalRepository
+     */
+    constructor(cliLogger, globalRepository)
+    {
+        super(cliLogger, globalRepository);
+
+        // Assign options
+        this._screenshot = new Screenshot();
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    static get injections()
+    {
+        return { 'parameters': [CliLogger, GlobalRepository] };
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    static get className()
+    {
+        return 'task/ScreenshotTask';
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    get sectionName()
+    {
+        return 'Creating screenshots';
+    }
+
+
+    /**
+     * @type {utils.Screenshot}
+     */
+    get screenshot()
+    {
+        return this._screenshot;
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    prepareParameters(buildConfiguration, parameters)
+    {
+        const parent = super.prepareParameters(buildConfiguration, parameters);
+        const scope = this;
+        const promise = co(function*()
+        {
+            const params = yield parent;
+            params.query = params.query || '*';
+            params.screenshotSkipTest = params.screenshotSkipTest || false;
+            return params;
+        }).catch(ErrorHandler.handler(scope));
+        return promise;
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    finalize(buildConfiguration, parameters)
+    {
+        this.screenshot.close();
+        return Promise.resolve([]);
+    }
+
+
+    /**
+     * @returns {Promise<VinylFile>}
+     */
+    createScreenshot(entity, width, url, filename)
+    {
+        const scope = this;
+        const promise = co(function*()
+        {
+            const work = scope.cliLogger.work('Rendering <' + entity.pathString + '/' + url + '> for viewport <' + width + '> as <' + filename + '>');
+            let screenshot = false;
+            try
+            {
+                screenshot = yield scope.screenshot.create(url, width);
+            }
+            catch (e)
+            {
+                // nop
+            }
+            if (screenshot)
+            {
+                const file = new VinylFile(
+                    {
+                        path: filename,
+                        contents: screenshot
+                    });
+                scope.cliLogger.end(work);
+                return file;
+            }
+            else
+            {
+                scope.cliLogger.end(work, 'Could not take screenshot');
+            }
+            return false;
+        });
+        return promise;
+    }
+
+
+    /**
+     * @returns {Promise<Array<VinylFile>>}
+     */
+    processEntity(entity, buildConfiguration, parameters)
+    {
+        /* istanbul ignore next */
+        if (!entity)
+        {
+            this.logger.warn(this.className + '::processEntity - No entity given');
+            return Promise.resolve(false);
+        }
+
+        const scope = this;
+        const promise = co(function *()
+        {
+            // Prepare
+            const result = [];
+            const params = yield scope.prepareParameters(buildConfiguration, parameters);
+
+            // Get suite
+            const testSuite = entity.testSuites.findBy(
+                {
+                    name: 'cssregression',
+                    site: entity.id.site
+                });
+
+            // Render screenshots
+            if (testSuite)
+            {
+                for (const testCase of testSuite.tests)
+                {
+                    // Reference
+                    if (fs.existsSync(testCase.referenceImagePath) == false)
+                    {
+                        const file = yield scope.createScreenshot(entity, testCase.viewportWidth, testCase.url, testCase.referenceImagePath);
+                        if (file)
+                        {
+                            result.push(file);
+                        }
+                    }
+                    // Test
+                    if (!params.screenshotSkipTest)
+                    {
+                        const file = yield scope.createScreenshot(entity, testCase.viewportWidth, testCase.url, testCase.testImagePath);
+                        if (file)
+                        {
+                            result.push(file);
+                        }
+                    }
+                }
+            }
+            return result;
+        }).catch(ErrorHandler.handler(scope));
+        return promise;
+    }
+}
+
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports.ScreenshotTask = ScreenshotTask;
